c52688693396b74a53f27935bd3305d079f12365
Amrin-Sumaiya <sabbirmd7677@gmail.com>
2025-05-05 12:37:48 +0600

    update

--- Backend/CONTROLLER/SubjectController.js
+++ Backend/CONTROLLER/SubjectController.js
@@ -38,3 +38,60 @@ export const getAllSubjects = async (req, res)=>{
 
 //get subject With specific Id Wise 
 
+export const  getSubjectById = async (req, res) =>{
+    try{
+        
+        const id = req.params.id
+        const subjectExist = await Subject.findById(id);
+        if (!subjectExist) {
+            return res.status(404).json({ message: "Subject Not Found. "});
+
+        }
+        res.status(200).json(subjectExist);
+
+    } catch( error){
+        res.status(500)({ errorMessage: error.message})
+    }
+}
+
+//update subjects data
+
+export const update = async (req, res) => {
+    try {
+        const id = req.params.id
+        const subjectExist = await Subject.findById(id);
+
+        if (!subjectExist) {
+            return res.status(404).json({ message: "Subject Not Found." });
+        }
+
+        const updatedData = await Subject.findByIdAndUpdate(id, req.body, {
+            new: true,
+        });
+
+        res.status(200).json(updatedData);
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+};
+
+//delete subjects data
+
+export const deleteSubjectData = async(req, res) => {
+    try{
+
+        const id = req.params.id
+        const subjectExist = await Subject.findById(id);
+        if (!subjectExist) {
+            return res.status(404).json({ message: "Subject Not Found. "});
+
+        }
+        await Subject.findByIdAndDelete(id);
+        res.status(200).json({ message: "Subject Delete Successfully"})
+
+    } catch( error){
+        res.status(500).json({ errorMessage: error.message})
+    }
+}
+
+
--- Backend/ROUTE/SubjectRoute.js
+++ Backend/ROUTE/SubjectRoute.js
@@ -1,10 +1,13 @@
 import express from "express"
 
-import {create, getAllSubjects} from "../CONTROLLER/SubjectController.js"
+import {create, deleteSubjectData, getAllSubjects, getSubjectById, update} from "../CONTROLLER/SubjectController.js"
 
 const route = express.Router();
 
 route.post("/subjects", create)
 route.get("/all_subjects", getAllSubjects)
+route.get("/specific_subject/:id", getSubjectById)
+route.put("/update/:id", update)
+route.delete("/delete_subject/:id", deleteSubjectData)
 
 export default route;
